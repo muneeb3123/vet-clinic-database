e453480aca230851846d284022db0ade3bab450b
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-06-13 21:45:55 +0500

    Insert data

---
+++ data.sql
@@ -0,0 +1,13 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Gabumon', '2018-11-15', 8, true, 2);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Pikachu', '2021-01-07',15.04, false, 1);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Devimon', '2017-05-12',11,true,5);
