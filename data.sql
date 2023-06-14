592fa995ab1230e853bc9cadc0ebfb52fa46f3e5
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-06-14 13:13:14 +0500

    insert more rows

--- data.sql
+++ data.sql
@@ -11,3 +11,25 @@ VALUES ('Pikachu', '2021-01-07',15.04, false, 1);
 
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '2017-05-12',11,true,5);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Charmender', '2020-02-08',-11,false,0);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Plantmon', '2021-11-15',-5.7,true,2);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Squirtle', '1993-04-02',-12.13,false,3);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Angemon', '2005-06-12',-45,true,1);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Boarmon', '2005-06-07',20.4,true,7);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Blossom', '1998-10-13',17,true,3);
+
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Ditto', '2022-05-14',22,true,4);
+
