ec680d777550d710d48c14df07114738c0068aed
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-06-14 13:55:07 +0500

    Add more queries

--- queries.sql
+++ queries.sql
@@ -17,3 +17,8 @@ SAVEPOINT sp1;
 UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO sp1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT COUNT(*) AS TOTAL_ANIMALS FROM animals;
+SELECT COUNT(*) AS Never_Escaped FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) AS Average_weight FROM animals;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) AS Average_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
