88d2a3d49243d4025cdc656875ec47de8112a299
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-06-15 13:01:54 +0500

    Link animals table with other tables

--- queries.sql
+++ queries.sql
@@ -22,3 +22,19 @@ SELECT COUNT(*) AS Never_Escaped FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) AS Average_weight FROM animals;
 SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) AS Average_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+UPDATE animals SET species_id = CASE 
+WHEN name LIKE '%mon'THEN (SELECT id FROM species WHERE name = 'Digimon')
+ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END;
+
+UPDATE animals
+SET owner_id = 
+  CASE 
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  END;
+
